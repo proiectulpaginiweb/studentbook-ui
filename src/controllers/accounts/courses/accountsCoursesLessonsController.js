00117a6fcb7bc74f5b5e519e115c303aa090c93c
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2016-06-12 18:34:55 +0300

    Add attendee code input to lesson tpl html

--- src/controllers/accounts/courses/accountsCoursesLessonsController.js
+++ src/controllers/accounts/courses/accountsCoursesLessonsController.js
@@ -14,7 +14,7 @@
           title: 'Curs 1',
           attended: 1,
           description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet saepe in qui adipisci, optio aliquam culpa, excepturi repellendus, id numquam delectus magnam sequi non ratione. Minus unde ab assumenda, beatae minima eaque expedita at tempora voluptatem quisquam. Commodi ratione quia tempora nobis debitis, minus voluptatum voluptates dolorum assumenda aliquid, ipsam.',
-          userAttended: true
+          userAttended: false
         };
 
         $scope.start = function() {
