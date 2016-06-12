f030f52250e48dfc37443a7e761e7441d8bdd4a8
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2016-06-12 22:30:32 +0300

    Add semi functionality for adding comments and submitting attendance

--- src/controllers/accounts/courses/accountsCoursesLessonsController.js
+++ src/controllers/accounts/courses/accountsCoursesLessonsController.js
@@ -9,12 +9,37 @@
       function($scope, cfpLoadingBar, $timeout) {
         $scope.dataIsLoaded = false;
 
+        $scope.newComment = {};
+        $scope.attendance = null;
+
+        $scope.addAttendance = function() {
+          $scope.lesson.userAttendance = 1;
+        };
+
+        $scope.addComment = function() {
+          $scope.newComment.gravatar = '00000000000000000000000000000000';
+          $scope.newComment.fullName = 'John Branch';
+          $scope.newComment.date     = new Date().getTime();
+
+          $scope.lesson.comments.push($scope.newComment);
+
+          $scope.newComment = {};
+        };
+
         $scope.lesson = {
           id: 1,
           title: 'Curs 1',
           attended: 20,
           description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet saepe in qui adipisci, optio aliquam culpa, excepturi repellendus, id numquam delectus magnam sequi non ratione. Minus unde ab assumenda, beatae minima eaque expedita at tempora voluptatem quisquam. Commodi ratione quia tempora nobis debitis, minus voluptatum voluptates dolorum assumenda aliquid, ipsam.',
-          userAttendance: -1
+          userAttendance: 0,
+          comments: [
+            {
+              gravatar: '00000000000000000000000000000000',
+              fullName: 'John Branch',
+              content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, deleniti. Minima, enim blanditiis, repudiandae quam quaerat expedita vitae saepe dignissimos.',
+              date: 1465751990000
+            }
+          ]
         };
 
         $scope.start = function() {
