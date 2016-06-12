c9585e23cf4c88cdd9e412b7c52e4c8d6512a902
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2016-06-12 14:26:09 +0300

    Add usersCoursesLessonsController and update show.tpl.html

---
+++ src/controllers/usersCoursesLessonsController.js
@@ -0,0 +1,34 @@
+(function(){
+  'use strict';
+
+  angular.module('studentbook.controllers')
+    .controller('usersCoursesLessonsController', [
+      '$scope',
+      'cfpLoadingBar',
+      '$timeout',
+      function($scope, cfpLoadingBar, $timeout) {
+        $scope.dataIsLoaded = false;
+
+        $scope.lesson = {
+          id: 1,
+          title: 'Curs 1',
+          description: 'Lorem ipsum dolor sit amet.'
+        };
+
+        $scope.start = function() {
+          cfpLoadingBar.start();
+        };
+
+        $scope.complete = function () {
+          cfpLoadingBar.complete();
+        };
+
+        $scope.start();
+        $timeout(function() {
+          $scope.dataIsLoaded = true;
+          $scope.complete();
+        }, 1000);
+      }
+    ]);
+
+})();
